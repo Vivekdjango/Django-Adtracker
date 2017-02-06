e31645284b708d6df4a229c931985fb8ee601453
Vivek Sinha <vivek.sinha@inmobi.com>
2017-02-06 16:04:52 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,4 +4,11 @@ set -e
 
 render-template \
   --addenvvars True \
-  --template pass.template
+  --template pass.template \
+  --output techstop/techstop/settings.py
+  
+render-template \
+  --addenvvars True \
+  --template pass.template \
+  --output techstop/tech/views.py
+  

aa5d3298fbe113e48fa6ecfcc804367d523ae640
Vivek Sinha <vivek.sinha@inmobi.com>
2017-02-07 14:32:05 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-render-template \
+/opt/inmobi/commons/mustache-util/render-template \
   --addenvvars True \
   --template techtop/techstop/tech_template/settings.py.template \
   --output techstop/techstop/settings.py
   
-render-template \
+/opt/inmobi/commons/mustache-util/render-template \
   --addenvvars True \
   --template techtop/techstop/tech_template/views.py.template \
   --output techstop/tech/views.py

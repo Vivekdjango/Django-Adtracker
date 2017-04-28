5d835fcaab6fe7da06e93300bedd37c736e32ca0
vivek.sinha <viveksinha@IC0532-L0.corp.inmobi.com>
2017-04-28 11:33:17 +0530

    New files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Tech-stop
-
-Techstop project to automate techstop ticket creation for Walk-ups issues.
--- techstop/tech/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-SetEnvIf X-Url-Scheme https HTTPS=1 
-
-# Create your tests here.
